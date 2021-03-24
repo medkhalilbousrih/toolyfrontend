6a70206e81dd0af732cc63db30931d6e946c44c4
Khalil <medkhalilbousrih@gmail.com>
2021-03-24 23:27:12 +0100

    navbar

--- src/components/thesupplier/Supplier.js
+++ src/components/thesupplier/Supplier.js
@@ -1,13 +1,20 @@
 import styled from "styled-components";
-
 import Info from "./Info";
 import SupplierNav from "./SupplierNav";
-
+import AddTool from "./AddTool";
+import { Route, Switch } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
 const Profile = () => {
+  let { path, url } = useRouteMatch();
   return (
     <Container>
       <Info />
       <SupplierNav />
+      <Switch>
+        <Route path={`${path}/addtool`}>
+          <AddTool />
+        </Route>
+      </Switch>
     </Container>
   );
 };
--- src/components/thesupplier/SupplierNav.js
+++ src/components/thesupplier/SupplierNav.js
@@ -1,5 +1,24 @@
+import { Navbar, Nav } from "react-bootstrap";
+import { Link, useRouteMatch } from "react-router-dom";
+
 const SupplierNav = () => {
-  return <div>aaa</div>;
+  let { path, url } = useRouteMatch();
+  return (
+    <Navbar bg="light" variant="light">
+      <Nav>
+        <Link to={`${url}/addtool`} className="nav-link">
+          Add Tool
+        </Link>
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default SupplierNav;
